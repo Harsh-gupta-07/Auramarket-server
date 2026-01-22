c2d6b7adfe35b480e75216f15620aa95aa7b2046
Harshvardhan Gupta <harshvardhangofficial@gmail.com>
2026-01-22 10:43:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/health",(req,res)=>{
+  res.status(200).json({"status":"Workings"})
+})
+
 app.use("/api/auth", authRoutes);
 app.use("/api", productRoutes);
 app.use("/api", userRoutes);
